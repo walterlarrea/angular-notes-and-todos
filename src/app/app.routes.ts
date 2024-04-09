f5e1391ede3c8f9a806f112638b81ed321ca412a
Walter Larrea <walterlarrea@live.com>
2024-04-08 23:40:21 -0300

    add: url redirect & auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AppLoginComponent } from './app-login/app-login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { authGuard } from './guard/auth/auth.guard';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+  },
   { path: 'login', component: AppLoginComponent },
-  { path: '', component: AppLoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
